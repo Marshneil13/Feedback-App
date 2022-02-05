f289f716fa5ca772effa5ce0889e2eb8ed675bb3
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-02-05 16:55:20 +0530

    FeedbaclItem file

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import { FaTimes } from 'react-icons/fa'
+import PropTypes from 'prop-types';
+import Card from './shared/Card';
 
-function FeedbackItem() {
-    const [rating, setRating] = useState(7);
-    const [text, setText] = useState('This is a feedback item');
-    const handleClick = () => {
-        setRating((prev) => {
-            console.log(prev);
-            return (prev+1);
-        });
-    }
-  return <div className='card'>
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
-  </div>;
+function FeedbackItem({item, handleDelete}) {
+    
+// const handleClick = (id =>{
+//   console.log(id);
+// })
+
+  return <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color='purple'/>
+      </button>
+      <div className="text-display">{item.text}</div>
+  </Card>;
+}
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem;
