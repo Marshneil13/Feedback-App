1bffc924f345348b5f885d6ace783fd19df87a95
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-02-20 13:24:52 +0530

    NavLink useParams useNavigate Nested Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {v4 as uuidv4} from 'uuid'
 import { useState } from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import feedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
+import Post from './components/Post';
 
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
@@ -20,15 +24,21 @@ function App() {
   }
 
   return (
-    <>
-    <Header/>
-    <div className="container">
-      <FeedbackForm handleAdd={addFeedback}/>
-      <FeedbackStats feedback={feedback}/>
-      <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-      
-    </div>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm handleAdd={addFeedback}/>
+              <FeedbackStats feedback={feedback}/>
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+            </>
+          } />
+          <Route exact path='/about' element={<AboutPage />}/>
+          <Route exact path='/post/*' element={<Post/>}/>
+      </Routes>
+      <AboutIconLink/>
+    </Router>
   );
 }
 
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,19 +1,21 @@
 const feedbackData = [
-    {
-    id: 1,
-    rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  {
+    id: 3,
+    rating: 8,
+    text: 'This is feedback item 3 coming from the backend.',
   },
   {
     id: 2,
     rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
+    text: 'This is feedback item 2 coming from the backend.',
+  },  
   {
-    id: 3,
-    rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    id: 1,
+    rating: 10,
+    text: 'This is feedback item 1 coming from the backend.',
   }
+  
+  
 ]
 
 export default feedbackData;
