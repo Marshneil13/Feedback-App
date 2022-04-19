12aa13f1ed4557c72335d259c24e8ef836b6cee5
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-04-19 18:56:26 +0530

    deploy via Heroku

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {v4 as uuidv4} from 'uuid'
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
